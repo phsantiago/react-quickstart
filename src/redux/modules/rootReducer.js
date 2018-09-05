a8fcd3c4da2b6fcd9b2768e1a1c60f5d8bae13e5
Pedro <pedroh_s_@hotmail.com>
2018-09-05 10:29:03 -0300

    :wrench: redux

---
+++ src/redux/modules/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+
+const initialState = {};
+const mock = (state = initialState, action) => state
+
+export default combineReducers({
+  mock
+})
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createBrowserHistory } from 'history'
 import { applyMiddleware, compose, createStore } from 'redux'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
-const history = createBrowserHistory()
+import reducers from './modules/rootReducer';
 
-const store = createStore(
-  connectRouter(history)(rootReducer), // new root reducer with router state
+export const history = createBrowserHistory()
+
+export default createStore(
+  connectRouter(history)(reducers), // new root reducer with router state
   {},
   compose(
     applyMiddleware(
