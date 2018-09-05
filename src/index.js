a1c98e537d9a007700210c42fe5d9fb4d3f366c6
Pedro <pedroh_s_@hotmail.com>
2018-09-05 10:28:45 -0300

    :wrench: router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import { Route, Switch } from 'react-router' // react-router v4
+import { ConnectedRouter } from 'connected-react-router'
+import store, { history } from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <Switch>
+          <Route exact path="/a" render={() => (<div>Match</div>)} />
+          <Route render={() => (<App />)} />
+        </Switch>
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
